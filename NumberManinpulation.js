94374a8b84aa8948ed31483cc6158074ef0f7678
Azizur Rahman <azizur.ice@gmail.com>
2015-05-30 15:01:52 -0700

    Added a file for adding two numbers

---
+++ NumberManinpulation.js
@@ -0,0 +1,20 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Ctrl+R),
+ * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
+ * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
+ */
+
+/*
+ * Assingment-1: Write a JavaScrpt program that will take/read two numbers from Keyboard and display the addition of those two numbers on the screen.
+ *
+ */
+
+var a, b, sum;
+a=parseInt(prompt("Enter the first number :",""));
+b=parseInt(prompt("Enter the second number :",""));
+sum=a+b;
+alert("The sum of "+ a +" and "+ b +" is :"+sum);
+
