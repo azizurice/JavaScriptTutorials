db9f668d6ffc8e525281c6487c22abf8d3b005d0
Azizur Rahman <azizur.ice@gmail.com>
2015-05-30 16:11:14 -0700

    Solved the second assingment

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This is a repository for JavaScript Tutorial. It has been developed foucusing the needs of very beginner learner of JavaScript. This tutorial will show  not only the step-by-step procedure  for learning JavaScript at the beginner level when a learner has no or zero knowledge of any programming but also help to learn any programming language. 
+This is a repository for JavaScript Tutorial. It has been developed foucusing the needs of very beginner learner of JavaScript. This tutorial will show  not only the step-by-step procedure  for learning JavaScript at the beginner level when a learner has no or zero knowledge of any programming but also help to learn any programming language (usually, just what you need is to know the corresponding syntax in your choosen language). 
 
 
 * Just clone the project using the following instruction. *
--- scratch.js
+++ scratch.js
@@ -1,20 +1,13 @@
 /*
- * This is a JavaScript Scratchpad.
+ * Assingment-2: Write a JavaScrpt program that will take/read two numbers from Keyboard and display the division (first nubmer by second number) on the screen.
  *
- * Enter some JavaScript, then Right Click or choose from the Execute Menu:
- * 1. Run to evaluate the selected text (Ctrl+R),
- * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
- * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
- */
-
-/*
- * Assingment-1: Write a JavaScrpt program that will take/read two numbers from Keyboard and display the addition of those two numbers on the screen.
+ * Try by yourself: similar type of projects like for subtraction, multiplication, or more than two numbers. 
  *
- */
+ /
 
-var a, b, sum;
+var a, b, division;
 a=parseInt(prompt("Enter the first number :",""));
 b=parseInt(prompt("Enter the second number :",""));
-sum=a+b;
-alert("The sum of "+ a +" and "+ b +" is :"+sum);
+division=a/b;
+alert("The division of "+ a +" by "+ b +" is :"+division);
 
