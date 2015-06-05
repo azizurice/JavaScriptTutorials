52b7470c6e7b2457d9efd1e75733ad827f87f9b4
iMaruf <Kamruzzamanmaruf@gmail.com>
2015-06-05 23:43:15 +0600

    Update scratch.js

--- scratch.js
+++ scratch.js
@@ -1,17 +1,17 @@
 /*
- * Assingment-3: Write a JavaScrpt program that will take/read two numbers from Keyboard and display the larger number on the screen.
+ * Assingment-3: Write a JavaScrpt program that will take/read two numbers from Keyboard and display the large number on the screen.
  *
  */
 
-var a, b, larger;
+var a, b, large;
 
 a=parseInt(prompt("Enter the first number :",""));
 b=parseInt(prompt("Enter the second number :",""));
 
 if(a>b){
-  larger=a;
+  large=a;
 }else{
-  larger=b;
+  large=b;
 }
 
-alert("The number is " + a + " and " + b + " larger number " + larger);
+alert("The number is " + a + " and " + b + " large number " + large);
