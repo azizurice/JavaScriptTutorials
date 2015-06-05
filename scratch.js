e833331b8caf34233b751e30f24601f650e7f33e
Mkz maruf <kamruzzamanmaruf@gmail.com>
2015-06-05 22:13:36 +0600

    Updated scratch.js for minus first number to second number.

--- scratch.js
+++ scratch.js
@@ -1,13 +1,13 @@
 /*
- * Assingment-3: Write a JavaScrpt program that will take/read two numbers from Keyboard and display the multiplication (first nubmer and second number) on the screen.
+ * Assingment-3: Write a JavaScrpt program that will take/read two numbers from Keyboard and display the minus (first nubmer to second number) on the screen.
  *
  */
 
-var a, b, multiplication;
+var a, b, minus;
 a=parseInt(prompt("Enter the first number :",""));
 b=parseInt(prompt("Enter the second number :",""));
-multiplication=a*b;
-alert("The multiplication of "+ a +" and "+ b +" is :"+multiplication);
+minus=a-b;
+alert("The minus of "+ a +" to "+ b +" is :"+minus);
 
 
 /*
