b70f94f8e754d02bacafbe17ea1b293ed7744404
Mkz maruf <kamruzzamanmaruf@gmail.com>
2015-06-07 21:42:36 +0600

    Updeted scratch.js for finding the largest number among four numbers.

--- scratch.js
+++ scratch.js
@@ -1,26 +1,42 @@
 /*
- * Assingment-3: Write a JavaScrpt program that will take/read three numbers from Keyboard and display the larger number on the screen.
+ * Assingment-3: Write a simple JavaScrpt program that will take/read four numbers from Keyboard and display the largest number on the screen.
  */
 
-var a, b,c,d,larger;
+var a, b,c,d,largest;
 
 a=parseInt(prompt("Enter the first number :",""));
 b=parseInt(prompt("Enter the second number :",""));
 c=parseInt(prompt("Enter the third number :",""));
-
+d=parseInt(prompt("Enter the fourth number:",""));
 
 if(a>b){
   if(a>c){
-    larger=a;
+    if(a>d){
+      largest=a;
+    }else{
+      largest=d;
+    }
   }else{
-    larger=c;
+    if(c>d){
+      largest=c;
+    }else{
+      largest=d;
+    }
   }
 }else{
   if(b>c){
-    larger=b;
+    if(b>d){
+      largest=b;
+    }else{
+      largest=d;
+    }
   }else{
-    larger=c;
+    if(c>d){
+      largest=c;
+    }else{
+      largest=d;
+    }
   }
 }
 
-alert(a+ " , " + b + " and " + c +  " The larger number is " + larger);
+alert(a+ " , " + b + " , " + c + " and " + d + " The largest number is " + largest);
