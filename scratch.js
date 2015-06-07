6552146a2e290454900627d8b34309b40d51211f
Mkz maruf <kamruzzamanmaruf@gmail.com>
2015-06-07 17:31:33 +0600

    Updated scratch.js for finding the largest number among three numbers

--- scratch.js
+++ scratch.js
@@ -1,17 +1,26 @@
 /*
- * Assingment-3: Write a JavaScrpt program that will take/read two numbers from Keyboard and display the larger number on the screen.
- *
+ * Assingment-3: Write a JavaScrpt program that will take/read three numbers from Keyboard and display the larger number on the screen.
  */
 
-var a, b, larger;
+var a, b,c,d,larger;
 
 a=parseInt(prompt("Enter the first number :",""));
 b=parseInt(prompt("Enter the second number :",""));
+c=parseInt(prompt("Enter the third number :",""));
+
 
 if(a>b){
-  larger=a;
+  if(a>c){
+    larger=a;
+  }else{
+    larger=c;
+  }
 }else{
-  larger=b;
+  if(b>c){
+    larger=b;
+  }else{
+    larger=c;
+  }
 }
 
-alert("The number is " + a + " and " + b + " larger number " + larger);
+alert(a+ " , " + b + " and " + c +  " The larger number is " + larger);
